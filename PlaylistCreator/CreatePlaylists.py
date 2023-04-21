54badee8d4c58b5f1dce3c03b9c44f6784bfe043
dalexandropoulos <alexandropoulos.dimitrios@gmail.com>
2023-04-21 04:41:37 +0300

    master

--- PlaylistCreator/CreatePlaylists.py
+++ PlaylistCreator/CreatePlaylists.py
@@ -32,3 +32,6 @@ extensions = ('.mp3', '.mp4', '.avi', '.mkv', '.flac')
 copyright = 'Copyright (c) 2023 Alexandropoulos Dimitrios'
 current_folder_path = os.getcwd()
 create_playlists_recursive(current_folder_path, extensions, copyright)
+
+
+# Testing Done !
