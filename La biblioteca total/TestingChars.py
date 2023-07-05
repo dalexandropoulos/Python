44e6288d0ba0d0809b7be0e5297e7e1fb8805fe4
dalexandropoulos <alexandropoulos.dimitrios@gmail.com>
2023-07-05 23:03:00 +0300

    main

--- La biblioteca total/TestingChars.py
+++ La biblioteca total/TestingChars.py
@@ -2,6 +2,6 @@ import string
 
 Dictionary_Letters = (string.ascii_lowercase 
                      +string.digits
-                     +'.,!?:;@#%^&*()"')
+                     +'.,!?:;@#%^&*^~-_()"/<=>{[]}')
 
 print(Dictionary_Letters, len(Dictionary_Letters))
