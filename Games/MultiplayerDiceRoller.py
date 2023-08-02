e00dfec6fda8ee243ce3821e8677378385323efb
Dimitri's Laptop <alexandropoulos.dimitrios@gmail.com>
2023-08-03 00:21:28 +0300

    main

--- Games/MultiplayerDiceRoller.py
+++ Games/MultiplayerDiceRoller.py
@@ -1,17 +1,37 @@
 import random
+import os
 
 def roll_dice():
-    return random.randint(1,6)
+    return random.randint(1, 6)
 
+os.system('clear')
 while True:
-    ans = input('Give number of players(2-4): ')
+    ans = input('Give number of players (2-4): ')
 
     if ans.isdigit():
-        ans = int(ans)
-        if 2 <= ans <= 4:
-            print('ok')
+        players = int(ans)
+        if 2 <= players <= 4:
             break
         else:
             print('Give a number between 2 and 4')
+    else:
+        print('Wrong input, try again')
+
+scores = [0 for _ in range(players)]
+
+player = (player + 1) % players # Here!
+still_playing = True
+total, value = 0, 0 
+while still_playing:
+    ans = input('Roll (r) or Stop (s)?: ')
+    if ans.lower() == 'r':
+        value = roll_dice()
+        print('Rolled:', value)
+        if value == 1:
+            print('You rolled 1, you lost your turn')
+        total = total + value
+        print('Total score:', total)
+    elif ans.lower() == 's':
+        still_playing = False
     else:
         print('Wrong input, try again')
